64da0c7e979665332abc3945c722841184011d77
Braedon Gough <braedon@pleo.io>
2020-06-01 16:28:43 +0200

    Push leson examples

--- 03-.d.ts-and-generics/lesson/src/App.tsx
+++ 03-.d.ts-and-generics/lesson/src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { FunctionComponent, ChangeEvent } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const App = () => {
+interface Props {
+    firstName: string
+}
+
+const App: FunctionComponent<Props> = ({ children, firstName }) => {
+    const [value, setValues] = React.useState('')
+
+    const onSubmit = (event: React.FormEvent) => {
+        event.preventDefault()
+        console.log('form submitted', value)
+    }
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+        setValues(event.target.value)
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
+                <p onChange={() => {}}>{firstName} </p>
+                <form onSubmit={onSubmit}>
+                    <input type="text" value={value} onChange={onChange} />
+                </form>
+                {children}
             </header>
         </div>
     )
